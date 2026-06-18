f5216a2bc6c772f77ebba4091976d12ab828f4be
EmmaAur <EmmaNiemenmaa@live.com>
2026-06-18 19:17:03 +0300

    All is done. Finally. How beautifully poetic moment this is.

--- base.c
+++ base.c
@@ -16,11 +16,17 @@ int main(int argc, char* argv[]) {
     int kytkin = 1; //variable for while-loop
     char *rivi = NULL;
     size_t len = 0;
-    char basePath[] = "/usr/bin/"; //path to the executables
-    int pathSize = 0;
+    ssize_t read;
+    char *pathArray[100]; //array for paths
+    int pathSum = 2; //sum of the paths inside the array. Gets increased everytime path command adds more paths
     int accessCode = 0; //variable to hold access rights result
-    
+    //let's set the base paths in the pathArray
+    pathArray[0] = "/usr/bin/";
+    pathArray[1] = "/bin/";
+
     /* ----------------------------------------------------------------------- */
+    /* Start: */
+    // Initializing variables
     char error_message[30] = "An error has occurred.\n";
     
     // For built in commands
@@ -30,6 +36,7 @@ int main(int argc, char* argv[]) {
     int cd_cmp;
     int path_cmp;
     int exit_cmp;
+    /* End */
     /* ----------------------------------------------------------------------- */
 
     //no arguments (except program name) = interactive mode
@@ -40,8 +47,10 @@ int main(int argc, char* argv[]) {
             
             if (strstr(rivi, " ") == NULL) { //IF there are no spaces (parameters) given for a command
                 char *options[100];
+                char path[100];
 
                 /* ----------------------------------------------------------------------- */
+                /* Start */
                 // Built in commands
 
                 rivi[strlen(rivi)-1] = '\0';
@@ -60,23 +69,33 @@ int main(int argc, char* argv[]) {
 				}
                 
                 rivi[strlen(rivi)] = '\n';
+                /* End */
                 /* ----------------------------------------------------------------------- */
 
-                pathSize = strlen(basePath) + strlen(rivi) + 1; //'+1' for '\0'
-                char path[pathSize]; //path to the specific command
-                snprintf(path, sizeof(path), "%s%s", basePath, rivi); //concatenating the strings into one specific path
-                //printf("%s", path);
-                path[strlen(path)-1] = '\0'; //stripping the newline-character from the path
+                rivi[strlen(rivi)-1] = '\0'; //adding null-terminator to the command
+            
+                for (int i = 0; i < pathSum; i++) {
+                    snprintf(path, 100, "%s%s", pathArray[i], rivi); //concatenating the strings to create final paths
+                    //printf("%s", path);
+
+                    accessCode = access(path, X_OK); //check for execution
+                    if (accessCode == -1) {
+                        printf("Not executable. Error number %d\n", errno);
+                        if (i == pathSum-1) {
+                            printf("Error: Command not found in any path.\n");
+                            exit(1);
+                        }
+                        continue;
+                    } else {
+                        printf("Executable.\n");
+                        break;
+                    }
+                }
+
+                //adding the arguments to the options
                 options[0] = path;
                 options[1] = '\0';
-            
-                accessCode = access(path, X_OK); //check for execution
-                if (accessCode == -1) {
-                    printf("Not executable. Error number %d\n", errno);
-                    continue;
-                } else {
-                    printf("Executable.\n");
-                }
+                
 
                 pid_t child_pid = fork();
                 if (child_pid == -1) {
@@ -94,7 +113,6 @@ int main(int argc, char* argv[]) {
                     waitpid(child_pid, NULL, 0);
                     printf("Child process is done.\n");
                 }
-            
             } else { //IF there are spaces, meaning there are parameters given
                 //first-time initiliazations and such are done before the while-loop
                 char *delimiter = strtok(rivi, " ");
@@ -104,13 +122,13 @@ int main(int argc, char* argv[]) {
                 char *options[100]; //we don't know how many options are given, but it is very unlikely the size 100 is exceeded
                 int a = 1;
                 int statusCode = 0;
+                char path[100];
                 
                 char *pathPtr = delimiter; //let's save the actual name of the command into its own variable
-                char path[100];
-                snprintf(path, sizeof(path), "%s%s", basePath, pathPtr); //so this is the actual command path without any options
 
                 options[0] = path; //execv wants the first argument to be the executable command
-               
+
+               //in this while loop we grab the command arguments
                 while (kytkin2 == 1) {
                     if ((delimiter = strtok(NULL, " ")) == NULL) {
                         kytkin2 = 0;
@@ -127,8 +145,10 @@ int main(int argc, char* argv[]) {
                 }
 
                 /* ----------------------------------------------------------------------- */
-                // Built in commands
+                /* Start */
+                // Built in commands, with arguments
 
+                // Functionality for built-in cd command
                 cd_cmp = strcmp(pathPtr, cd_str);
 				if ((cd_cmp == 0) && (a == 2)) {
                     int chd = chdir(options[1]);
@@ -141,23 +161,52 @@ int main(int argc, char* argv[]) {
                     //printf("Error reading [cd] command. [cd] requires 1 argument: cd /new/path\n");
                     continue;
                 }
-                
+                // Functionality for built-in path command
                 path_cmp = strcmp(pathPtr, path_str);
-				if ((path_cmp == 0) && (a == 1)) {
-					printf("path successful!\n");
-                    continue;
-				} else if ((path_cmp == 0) && (a != 1)) {
-                    write(STDERR_FILENO, error_message, strlen(error_message));
-					//printf("Error reading [path] command.\n");
+				if ((path_cmp == 0) && (a >= 1)) {
+                    // Empty the previous paths from the pathArray
+                    for (int i = 0; i < pathSum; i++) {
+                        pathArray[i] = NULL;
+                    }
+                    pathSum = 0; // Set pathSum to zero because there are currently no paths
+                    // Fill the pathArray with the new path(s)
+                    for (int i = 0; i < a-1; i++) {
+                        pathArray[i] = options[i+1];
+                        pathSum++;
+                    }
 				    continue;
                 }
+
+                /* End */
                 /* ----------------------------------------------------------------------- */
+
+                for (int i = 0; i < pathSum; i++) {
+                    snprintf(path, 100, "%s%s", pathArray[i], pathPtr); //concatenating the strings to create final paths
+
+                    //printf("%s", path);
+
+                    accessCode = access(path, X_OK); //check for execution
+                    if (accessCode == -1) {
+                        printf("Not executable. Error number %d\n", errno);
+                        if (i == pathSum-1) {
+                            printf("Error: Command not found in any path.\n");
+                            exit(1);
+                        }
+                        continue;
+                    } else {
+                        printf("Executable.\n");
+                        break;
+                    }
+                }
+
+                options[0] = path; //execv wants the first argument to be the executable command
 
                 options[a] = '\0'; //execv requires null terminator at the end of array
 
                 /**for (int i=0; i<a; i++) {
                     printf("%s", options[i]);
                 }*/
+
                 pid_t child_pid = fork();
                 if (child_pid == -1) {
                     perror("Error creating process.\n");
@@ -176,5 +225,197 @@ int main(int argc, char* argv[]) {
                 }
             }
         }
+    } else if (argc == 2) // two arguments = batch mode
+    {
+        FILE *Tiedosto = NULL;
+        char *fname = argv[1];
+        if ((Tiedosto = fopen(fname, "r")) == NULL) {
+            printf("Error while opening file. Check filename.\n");
+            exit(1);
+        }
+        while ((read = getline(&rivi, &len, Tiedosto)) != -1) { //the code for the next if-else statement is the same as in interactive mode
+            if (strstr(rivi, " ") == NULL) { //IF there are no spaces (parameters) given for a command
+                char *options[100];
+
+                char path[100];
+                
+                /* ----------------------------------------------------------------------- */
+                /* Start */
+                // Built in commands
+
+                rivi[strlen(rivi)-1] = '\0';
+                
+                // Check if the user wants to exit the shell program
+                exit_cmp = strcmp(exit_str, rivi);
+                if (exit_cmp == 0) {
+					exit(0);
+				}
+                
+                // Check if cd was called without parameters
+                cd_cmp = strcmp(cd_str, rivi);
+                if (cd_cmp == 0) {
+					write(STDERR_FILENO, error_message, strlen(error_message));
+                    continue;
+				}
+                
+                rivi[strlen(rivi)] = '\n';
+                /* End */
+                /* ----------------------------------------------------------------------- */
+
+                rivi[strlen(rivi)-1] = '\0'; //adding null-terminator to the command
+            
+                for (int i = 0; i < pathSum; i++) {
+                    snprintf(path, 100, "%s%s", pathArray[i], rivi); //concatenating the strings to create final paths
+                    //printf("%s", path);
+
+                    accessCode = access(path, X_OK); //check for execution
+                    if (accessCode == -1) {
+                        printf("Not executable. Error number %d\n", errno);
+                        if (i == pathSum-1) {
+                            printf("Error: Command not found in any path.\n");
+                            exit(1);
+                        }
+                        continue;
+                    } else {
+                        printf("Executable.\n");
+                        break;
+                    }
+                }
+
+                //adding the arguments to the options
+                options[0] = path;
+                options[1] = '\0';
+
+                pid_t child_pid = fork();
+                if (child_pid == -1) {
+                    perror("Error creating process.\n");
+                }
+
+                if (child_pid == 0) {
+                    //child process code
+                    int statusCode = execv(path, options);
+
+                    perror("Execv has failed.\n"); //if reaches this code, execv has failed
+                    exit(1);
+                } else {
+                    //parent process code
+                    waitpid(child_pid, NULL, 0);
+                    printf("Child process is done.\n");
+                }
+                
+            } else { //IF there are spaces, meaning there are parameters given
+                //first-time initiliazations and such are done before the while-loop
+                char *delimiter = strtok(rivi, " ");
+                //printf("Delimiter on %s\n", delimiter);
+                int kytkin2 = 1;
+                
+                char *options[100]; //we don't know how many options are given, but it is very unlikely the size 100 is exceeded
+                int a = 1;
+                int statusCode = 0;
+                char path[100];
+                
+                char *pathPtr = delimiter; //let's save the actual name of the command into its own variable
+                
+
+                options[0] = path; //execv wants the first argument to be the executable command
+               
+                while (kytkin2 == 1) {
+                    if ((delimiter = strtok(NULL, " ")) == NULL) {
+                        kytkin2 = 0;
+                        continue;
+                    }
+                    //replace the newline if this is the last argument
+                    if (strstr(delimiter, "\n")) {
+                        delimiter[strcspn(delimiter, "\n")] = '\0';
+                    }
+
+                    options[a] = delimiter;
+                    //printf("Options %d on %s\n", a, options[a]);
+                    a++;
+                }
+
+
+                /* ----------------------------------------------------------------------- */
+                /* Start */
+                // Built in commands, with arguments
+
+                cd_cmp = strcmp(pathPtr, cd_str);
+				if ((cd_cmp == 0) && (a == 2)) {
+                    int chd = chdir(options[1]);
+                    if (chd != 0) {
+                        write(STDERR_FILENO, error_message, strlen(error_message));
+                    }
+                    continue;
+				} else if ((cd_cmp == 0) && (a != 2)) {
+					write(STDERR_FILENO, error_message, strlen(error_message));
+                    //printf("Error reading [cd] command. [cd] requires 1 argument: cd /new/path\n");
+                    continue;
+                }
+                
+                path_cmp = strcmp(pathPtr, path_str);
+				if ((path_cmp == 0) && (a >= 1)) {
+                    for (int i = 0; i < pathSum; i++) {
+                        pathArray[i] = NULL;
+                    }
+                    pathSum = 0;
+                    for (int i = 0; i < a-1; i++) {
+                        pathArray[i] = options[i+1];
+                        pathSum++;
+                    }
+				    continue;
+                }
+                /* Emd */
+                /* ----------------------------------------------------------------------- */
+
+
+                for (int i = 0; i < pathSum; i++) {
+                    snprintf(path, 100, "%s%s", pathArray[i], pathPtr); //concatenating the strings to create final paths
+
+                    //printf("%s", path);
+
+                    accessCode = access(path, X_OK); //check for execution
+                    if (accessCode == -1) {
+                        printf("Not executable. Error number %d\n", errno);
+                        if (i == pathSum-1) {
+                            printf("Error: Command not found in any path.\n");
+                            exit(1);
+                        }
+                        continue;
+                    } else {
+                        printf("Executable.\n");
+                        break;
+                    }
+                }
+
+                options[a] = '\0'; //execv requires null terminator at the end of array
+
+                /**for (int i=0; i<a; i++) {
+                    printf("%s", options[i]);
+                }*/
+
+                pid_t child_pid = fork();
+                if (child_pid == -1) {
+                    perror("Error creating process.\n");
+                }
+
+                if (child_pid == 0) {
+                    //child process code
+                    statusCode = execv(path, options);
+
+                    perror("Execv has failed.\n"); //if reaches this code, execv has failed
+                    exit(1);
+                } else {
+                    //parent process code
+                    waitpid(child_pid, NULL, 0);
+                    printf("Child process is done.\n");
+                }
+
+
+            } 
+        }
+    } else {
+        printf("Error. The shell can be invoked with only 1 or 2 arguments i.e. ./lush or ./lush filename.txt");
+        exit(1);
     }
+    
 }
